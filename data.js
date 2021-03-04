f484c30b13ff24c3ca39c6ddd5994a3735eedbf7
nvkhuong <vankhuong240499@gmail.com>
2021-03-04 23:39:23 +0700

    Tool Learn English

---
+++ data.js
@@ -0,0 +1,193 @@
+const question_1 = [
+  {
+    en:
+      "sure, i studied at cao thang technical college and i graduated in 2019 with a degree in automobile engineering.",
+    vi:
+      "chắc chắn rồi, tôi học trường cao đẳng kỹ thuật cao thắng và tôi tốt nghiệp năm 2019 với bằng kỹ sư ô tô.",
+  },
+  {
+    en:
+      'at school, i took part in "mini racing car" competition and some other volunteer activities such as green summer campaign, and blood donation.',
+    vi:
+      'ở trường, em tham gia cuộc thi "đua xe mini" và một số hoạt động tình nguyện khác như chiến dịch mùa hè xanh, hiến máu nhân đạo.',
+  },
+  {
+    en:
+      "in the five final year, i have had an internship at toyota factory for 6 months helps me get more experiences in repairing automobiles.",
+    vi:
+      "năm năm cuối, tôi đã có 6 tháng thực tập tại nhà máy toyota giúp tôi có thêm kinh nghiệm sửa chữa ô tô.",
+  },
+];
+
+const question_2 = [
+  {
+    en:
+      "a friend of mine saw the position listed on vietnamwork website and he forwarded it to me.",
+    vi:
+      "một người bạn của tôi đã nhìn thấy vị trí được ghi trên trang web vietnamwork và anh ấy đã chuyển tiếp cho tôi.",
+  },
+  {
+    en:
+      "so i spent time on your website learning about this position and your company.",
+    vi:
+      "vì vậy, tôi đã dành thời gian trên trang web của bạn để tìm hiểu về vị trí này và công ty của bạn.",
+  },
+];
+
+const question_3 = [
+  {
+    en:
+      "i am so impressed by your contributions to society every year and i want to become a volunteer in those meaningful activities.",
+    vi:
+      "tôi rất ấn tượng về những đóng góp của bạn cho xã hội hàng năm và tôi muốn trở thành tình nguyện viên trong những hoạt động ý nghĩa đó.",
+  },
+  {
+    en: "i also like the ways you support your employees.",
+    vi: "tôi cũng thích cách bạn hỗ trợ nhân viên của mình.",
+  },
+  {
+    en:
+      "i know that you pay for employees to develop the new techniques and skills.",
+    vi:
+      "tôi biết rằng bạn trả tiền để nhân viên phát triển các kỹ thuật và kỹ năng mới.",
+  },
+  {
+    en: "i think that really helps to build employee satisfaction and loyalty.",
+    vi:
+      "tôi nghĩ rằng điều đó thực sự giúp xây dựng sự hài lòng và lòng trung thành của nhân viên.",
+  },
+];
+
+const question_4 = [
+  { en: "i am good at teamwork.", vi: "tôi giỏi làm việc nhóm." },
+  {
+    en: "during my school time, i had many projects woking in group of 3 or 4.",
+    vi:
+      "trong thời gian đi học, tôi đã có nhiều dự án thực hiện theo nhóm 3 hoặc 4 người.",
+  },
+  {
+    en:
+      "we gave feedbacks to each other and helped other members to complete the projects more efficiently.",
+    vi:
+      "chúng tôi đã phản hồi cho nhau và giúp các thành viên khác hoàn thành dự án hiệu quả hơn.",
+  },
+  {
+    en: "thus, we had good results.",
+    vi: "vì vậy, chúng tôi đã có kết quả tốt.",
+  },
+  {
+    en: "besides, i am also a punctual person.",
+    vi: "bên cạnh đó, tôi cũng là một người rất đúng giờ.",
+  },
+  {
+    en:
+      "i always arrived early and completed my work on time in my internship.",
+    vi: "tôi luôn đến sớm và hoàn thành công việc đúng hạn trong kỳ thực tập.",
+  },
+];
+
+const question_5 = [
+  {
+    en: "to be honest, i am not good at public speaking.",
+    vi: "thành thật mà nói, tôi không giỏi nói trước đám đông.",
+  },
+  {
+    en: "i always feel nervous when making presentation in class.",
+    vi: "tôi luôn cảm thấy lo lắng khi thuyết trình trên lớp.",
+  },
+  {
+    en:
+      "in other to reduce the speaking anxiety, i am trying to present my idea in larger group.",
+    vi:
+      "ngoài ra, để giảm bớt lo lắng khi nói, tôi đang cố gắng trình bày ý tưởng của mình trong một nhóm lớn hơn.",
+  },
+  {
+    en: "moreover, i don't speak english very well.",
+    vi: "hơn nữa, tôi nói tiếng anh không tốt lắm.",
+  },
+  {
+    en:
+      "so i am talking part in an english communication course to improve my speaking skill.",
+    vi:
+      "vì vậy, tôi đang tham gia một khóa học giao tiếp tiếng anh để cải thiện kỹ năng nói của mình.",
+  },
+];
+
+const question_6 = [
+  {
+    en: "sure, when i was a student at cao thang college.",
+    vi: "chắc chắn rồi, khi tôi còn là sinh viên trường cao thắng.",
+  },
+  {
+    en: "i had 4 projects in the same week.",
+    vi: "tôi đã có 4 dự án trong cùng một tuần.",
+  },
+  { en: "in order to meet the deadline.", vi: "để đáp ứng thời hạn." },
+  {
+    en: "i created a schedule in details to manage my time.",
+    vi: "tôi đã tạo một lịch trình chi tiết để quản lý thời gian của mình.",
+  },
+  {
+    en: "i broke the projects up into smaller steps.",
+    vi: "tôi chia nhỏ các dự án thành các bước nhỏ hơn.",
+  },
+  { en: "as the result.", vi: "kết quả là." },
+  {
+    en:
+      "i finished all the projects and my teacher was happy with the work quality.",
+    vi:
+      "tôi đã hoàn thành tất cả các dự án và giáo viên của tôi rất vui với chất lượng công việc.",
+  },
+];
+
+const question_7 = [
+  {
+    en:
+      "over the next five years, my goals involve growing with a company where i can continue to learn, take on more responsibilities, and contribute as much value as i can.",
+    vi:
+      "trong năm năm tới, mục tiêu của tôi liên quan đến việc phát triển với một công ty nơi tôi có thể tiếp tục học hỏi, đảm nhận nhiều trách nhiệm hơn và đóng góp nhiều giá trị nhất có thể.",
+  },
+  {
+    en:
+      "i also want to speak english more fluently so that i can exchange the experience with the foreign mechanics.",
+    vi:
+      "tôi cũng muốn nói tiếng anh trôi chảy hơn để có thể trao đổi kinh nghiệm với các thợ máy nước ngoài.",
+  },
+];
+
+const question_8 = [
+  {
+    en:
+      "i think your company has already had the suitable salary policy for the worker.",
+    vi:
+      "tôi nghĩ quý công ty đã có chính sách lương phù hợp cho người lao động.",
+  },
+  {
+    en: "so i will follows its range.",
+    vi: "vì vậy, tôi sẽ làm theo phạm vi của nó.",
+  },
+];
+
+const question_9 = [
+  {
+    en: "yes, can you tell me what is the biggest challenge in this position.",
+    vi:
+      "vâng, bạn có thể cho tôi biết thách thức lớn nhất ở vị trí này là gì không?",
+  },
+  {
+    en: "1 do you have to work in shifts.",
+    vi: "1 bạn có phải làm việc theo ca.",
+  },
+  {
+    en: "2 do i have to work overtime.",
+    vi: "2 tôi có phải làm thêm giờ không.",
+  },
+  {
+    en: "3 do i often make business trips.",
+    vi: "3 tôi có thường xuyên đi công tác không.",
+  },
+  {
+    en: "4 can you tell me about the team that i would be working with.",
+    vi: "4 bạn có thể cho tôi biết về đội mà tôi sẽ làm việc cùng.",
+  },
+];
